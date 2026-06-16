72659d2b41ae6da0e46bb94023d06adae52877ee
Chris McKenzie <kristopolous@yahoo.com>
2026-06-16 00:30:48 -0700

    cleanup

--- ps.old/src/petsitter/backends/__init__.py
+++
@@ -1,9 +0,0 @@
-"""LLM backends for PetSitter."""
-
-from petsitter.backends.base import LLMBackend
-from petsitter.backends.ollama import OllamaBackend
-
-__all__ = [
-    "LLMBackend",
-    "OllamaBackend",
-]
--- ps.old/src/petsitter/harness/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Harness module for PetSitter task execution."""
-
-from petsitter.harness.loader import load_task, load_tasks_from_model
-from petsitter.harness.models import TaskConfig, Task
-
-__all__ = ["load_task", "load_tasks_from_model", "TaskConfig", "Task"]

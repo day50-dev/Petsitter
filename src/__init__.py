56855b8f823a8f59085525dd9c0166b140b1256f
Chris McKenzie <kristopolous@yahoo.com>
2026-07-04 10:47:05 -0700

    updating the pyproject

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,19 @@
-from src.trick import Trick, callmodel
+from src.trick import (
+    Trick,
+    build_modelset_example,
+    callmodel,
+    callmodel_sync,
+    configure_modelset,
+    get_model_config,
+    parse_mas_uri,
+)
 
-__all__ = ["Trick", "callmodel"]
+__all__ = [
+    "Trick",
+    "build_modelset_example",
+    "callmodel",
+    "callmodel_sync",
+    "configure_modelset",
+    "get_model_config",
+    "parse_mas_uri",
+]
